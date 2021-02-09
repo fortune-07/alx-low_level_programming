8336af1c86d42d262e8ad2f7bd8d867e011ab131
fortune-07 <fortuneniguel@gmail.com>
2021-02-09 09:04:32 +0000

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+for (int c = 97; c <= 122; ++c)
+cout << c << "";
+cout << end1; for (int c = 97; c <= 122; ++c)
+cout << c << "";
+cout << end1;
+}
+int _putchar(void)
+{
+print_alphabet(ch);
+return (0);
+}
