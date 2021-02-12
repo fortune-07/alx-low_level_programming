fbc537543d3564042f4b4170ce9bc682b0dcdbcf
fortune-07 <fortuneniguel@gmail.com>
2021-02-12 13:26:28 +0000

    0-isupper.c

---
+++ 0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - for uppercase letters.
+ *
+ * Return: Always 0.
+ */
+int _isupper(void)
+{
+int c;
+
+c = 'A';
+printf("%c\n", c);
+c = 'a';
+printf("%c\n", c);
+return (0);
+}
---
+++ holberton.h
@@ -0,0 +1,8 @@
+#ifndef holberton_h
+#define holberton_h
+
+#include <stdio.h>
+
+int _isupper(int c);
+
+#endif /* holberton_h */
