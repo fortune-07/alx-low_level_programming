0d44e46344f32be4d952c4656ec4cc16e4536a9c
fortune-07 <fortuneniguel@gmail.com>
2021-02-15 07:37:00 +0000

    made changes

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void reset_to_98(int *n);
+{
+int n;
+
+n = 402;
+printf("n=%d\n", n);
+reset_to_98(&n);
+printf("n=%d\n", n);
+return (0);
+}
