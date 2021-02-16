0f4b203c042313abd4723a56bde2d2eca5bccbef
fortune-07 <fortuneniguel@gmail.com>
2021-02-16 18:55:54 +0000

    made changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -7,12 +7,3 @@
  * Return: Always 0.
  */
 void reset_to_98(int *n);
-{
-int n;
-
-n = 402;
-printf("n=%d\n", n);
-reset_to_98(&n);
-printf("n=%d\n", n);
-return (0);
-}
--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -14,5 +14,6 @@ n = 402;
 printf("n=%d\n", n);
 reset_to_98(&n);
 printf("n=%d\n", n);
+reset_to_98(n);
 return (0);
 }
