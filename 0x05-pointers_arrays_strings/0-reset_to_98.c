5d96babaf367d75f1a686115ba13f4caa38afd2b
fortune-07 <fortuneniguel@gmail.com>
2021-02-19 14:07:32 +0000

    corrections on task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
-
 /**
- * reset_to_98 - set integer 402 to 98.
- *@n: parameter holding integer valueto be changed.
- * Return: void.
- */
-
+*reset_to_98 - function to reset an integer to another value.
+*@n: parameter holding an integer to be changed.
+*
+*Return: void.
+*
+**/
 void reset_to_98(int *n)
 {
 *n = 98;
