025e74fb5a51dff56b816971f39dbfa96cfbe77a
fortune-07 <fortuneniguel@gmail.com>
2021-02-23 08:51:31 +0000

    completed task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 /**
-* _memset - Fills the first n bytes of the memory area
-*      	pointed to by @s with the constant byte @c.
+* _memcpy - Fills the first n bytes of the memory area.
 * @src: A pointer to the memory area to be filled.
 * @dest: The character to fill the memory area with.
 * @n: The number of bytes to be filled.
@@ -10,15 +9,10 @@
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
-n = 0;
-char *temp;
+unsigned int i;
 
 for (i = 0; i < n; i++)
-temp[i] = src[i];
+*(dest + i) = *(src + i);
 
-for (i = 0; i < n; i++)
-dest[i] = temp[i];
-
-return (src);
+return (dest);
 }
