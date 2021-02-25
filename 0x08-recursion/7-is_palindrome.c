874382a019917a13754a77de7f0d72a03eec7610
fortune-07 <fortuneniguel@gmail.com>
2021-02-25 09:45:31 +0000

    completed task 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,5 +1,44 @@
 #include "holberton.h"
 /**
+* find_strlen - function to find the length.
+* @s: holds the value of the string.
+*
+* Return: 0.
+*/
+int find_strlen(char *s)
+{
+int len = 0;
+
+if (*(s + len))
+{
+len++;
+len += find_strlen(s + len);
+}
+return (len);
+}
+
+/**
+* check_palindrome - function to check palindrome.
+* @s: holds string value.
+* @len: value of length.
+* @i: holds value of i.
+*
+* Return: 0.
+*/
+int check_palindrome(char *s, int len, int i)
+{
+if (s[i] == s[len / 2])
+{
+return (1);
+}
+if (s[i] == s[len - i - 1])
+{
+return (check_palindrome(s, len, i + 1));
+}
+return (0);
+}
+
+/**
 * is_palindrome - returns 1 if a string is a palindrome and 0 if not.
 * @s: parameter string that holds value of an integer.
 *
@@ -7,17 +46,12 @@
 */
 int is_palindrome(char *s)
 {
-int i;
-int e;
+int i = 0;
+int len = find_strlen(s);
 
-if (i == e)
-return (true);
-
-if (s[i] != s[e])
-return (false);
-
-if (i < e + 1)
-return (is_palindrome(s, i + 1, e - 1)); 
-  
-return (true);
+if (!(*s))
+{
+return (1);
+}
+return (check_palindrome(s, len, i));
 }
