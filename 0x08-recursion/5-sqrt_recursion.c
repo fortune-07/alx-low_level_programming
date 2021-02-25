36585104e7289037657eea97b0b7338d3e1a990f
fortune-07 <fortuneniguel@gmail.com>
2021-02-25 09:59:59 +0000

    completed task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,21 @@
 #include "holberton.h"
 /**
+* isSqrt - function to find squareroot.
+* @n: holds value of n.
+* @i: holds value of i.
+*
+* Return: 0
+*/
+int isSqrt(int n, int i)
+{
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (isSqrt(n, i + 1));
+}
+
+/**
 * _sqrt_recursion - returns the natural square root of a number.
 * @n: holds value of the integer.
 *
@@ -7,10 +23,11 @@
 */
 int _sqrt_recursion(int n)
 {
-int i;
-if (i * i > n)
+int square = 2;
+
+if (n < 0)
 return (-1);
-if (i * i == n)
-return (i);
-return (_sqrt_recursion(n, i + 1));
+else if (n == 0 || n == 1)
+return (n);
+return (isSqrt(n, square));
 }
