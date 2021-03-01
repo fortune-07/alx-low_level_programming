6ad1c971c81c2fe811fed94623a652ef3da7ad64
fortune-07 <fortuneniguel@gmail.com>
2021-03-01 08:10:24 +0000

    completed task 0

---
+++ 0x09-static_libraries/test.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+void test(void)
+{
+printf("int _putchar(char c);");
+printf("int _islower(int c);");
+printf("int _isalpha(int c);");
+printf("int _abs(int n);");
+printf("int _isupper(int c);");
+printf("int _isdigit(int c);");
+printf("int _strlen(char *s);");
+printf("void _puts(char *s);");
+printf("char *_strcpy(char *dest, char *src);");
+printf("int _atoi(char *s);");
+printf("char *_strcat(char *dest, char *src);");
+printf("char *_strncat(char *dest, char *src, int n);");
+printf("char *_strncpy(char *dest, char *src, int n);");
+printf("int _strcmp(char *s1, char *s2);");
+printf("char *_memset(char *s, char b, unsigned int n);");
+printf("char *_memcpy(char *dest, char *src, unsigned int n);");
+printf("char *_strchr(char *s, char c);");
+printf("unsigned int _strspn(char *s, char *accept);");
+printf("char *_strpbrk(char *s, char *accept);");
+printf("char *_strstr(char *haystack, char *needle);");
+}
