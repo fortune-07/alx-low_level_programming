e8d1646872d17904329e7740cafa359dcd06f7db
fortune-07 <fortuneniguel@gmail.com>
2021-03-02 16:14:33 +0000

    updated task 5

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,44 +1,36 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
 /**
-* *argstostr - concatenates all arguements to the program
-* @ac: arguement count
-* @av: pointer to arguements
-* Return: pointer to new space in memory or null
-**/
+* *argstostr - concatenates all the arguments of the program
+* @ac: number of arguments
+* @av: array of arguments
+*
+* Return: Pointer to the new string (Success), NULL (Error)
+*/
 char *argstostr(int ac, char **av)
 {
-char *strDup;
-int i, j, k, size;
+int i, j, k;
+char *str;
 
 if (ac == 0 || av == NULL)
 return (NULL);
-size = 0;
-/* count the number of chars in each string */
-for (i = 0; i < ac; i++)
-{
-for (j = 0; av[i][j] != '\0'; j++)
-size++;
-size++;
-}
-size++;
 
-/**
-* allocate memory for total number of chars and
-* new line for each word
-*/
-strDup = malloc(sizeof(char) * size);
-if (strDup == NULL)
+str = (char *) malloc(sizeof(char *));
+
+if (str == NULL)
 return (NULL);
+
 k = 0;
+
 for (i = 0; i < ac; i++)
 {
 for (j = 0; av[i][j] != '\0'; j++)
 {
-strDup[k++] = av[i][j];
+str[k] = av[i][j];
+k++;
 }
-strDup[k++] = '\n';
+str[k] = '\n';
+k++;
 }
-strDup[k] = '\0';
-return (strDup);
+return (str);
 }
