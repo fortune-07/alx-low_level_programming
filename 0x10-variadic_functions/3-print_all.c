64ee2bbf3d98515b1a8248cc18a940dbee4428ac
fortune-07 <fortuneniguel@gmail.com>
2021-03-11 10:13:18 +0000

    updated task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,42 +5,41 @@
 */
 void print_all(const char * const format, ...)
 {
-int i = 0, flag;
-char *str;
+int i = 0;
+char *str, *sep = "";
 
 va_list list;
 
 va_start(list, format);
 
-while (format[i] && format != NULL)
+if (format)
 {
-flag = 1;
+while (format[i])
+{
 switch (format[i])
 {
 case 'c':
-printf("%c", va_arg(list, int));
+printf("%s%c", sep, va_arg(list, int));
 break;
 case 'i':
-printf("%d", va_arg(list, int));
+printf("%s%d", sep, va_arg(list, int));
 break;
 case 'f':
-printf("%f", va_arg(list, double));
+printf("%s%f", sep, va_arg(list, double));
 break;
 case 's':
 str = va_arg(list, char *);
 if (!str)
-{
-printf("(nil)");
-break;
-}
-printf("%s", str);
+str = "(nil)";
+printf("%s%s", sep, str);
 break;
 default:
-flag = 0;
-}
-if (format[i + 1] != '\0' && flag)
-printf(", ");
 i++;
+continue;
+}
+sep = ", ";
+i++;
+}
 }
 
 printf("\n");
