cc2a16740b2e761f34fbf99458c27bd2e85a25ab
fortune-07 <fortuneniguel@gmail.com>
2021-03-19 07:46:23 +0000

    updated task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
 extern printf
-ection .data
+	ection .data
 msg:	 db "Hello, Holberton", 0
 fmt:	 db "%s", 10, 0
 
-section .text
-global main
+	section .text
+	global main
 main:
-push rbp
-mov rdi,fmt
-mov rsi,msg
-mov eax,0
-call printf
+	push rbp
+	mov rdi,fmt
+	mov rsi,msg
+	mov rax,0
+	call printf
 
-pop rbp
-mov rax,0
-ret
+	pop rbp
+	mov rax,0
+	ret
